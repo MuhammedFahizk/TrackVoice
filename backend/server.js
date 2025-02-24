fc606b33978e63e71018b13551762f680d42d946
MuhammedFahizk <145779419+MuhammedFahizk@users.noreply.github.com>
2025-02-24 21:03:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 connectDB();
 
 app.use(cors({
-    origin: 'http://localhost:5175', 
+    origin: 'https://track-voice-gawe.vercel.app', 
     credentials: true,
   }));
   app.set('view engine', 'ejs');
